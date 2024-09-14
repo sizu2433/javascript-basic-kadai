6bcc02e5e0317d544d91d82d8f5359ebe0307289
sizu2433 <sizuki791@gmail.com>
2024-09-14 14:48:31 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,10 @@
+const nijou = (num) => {
+  const answer = num * num;
+  return answer; // 
+};
+
+const result1 = nijou(33);
+console.log(result1);
+
+const result2 = nijou(233);
+console.log(result2);
